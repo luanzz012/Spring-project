e985f67a96cfb7bc63a0308298a1c9c8ba030159
luanzz012 <luangonzagaoliveira@gmail.com>
2025-08-14 04:47:13 -0300

    Controller criado

---
+++ src/main/java/com/luanliv/SpringProject/Controller.java
@@ -0,0 +1,16 @@
+package com.luanliv.SpringProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Bem vindo(a)!";
+
+    }
+}
