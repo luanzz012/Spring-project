df327867b8a472d2c1017b34444e87ef8f867472
luanzz012 <luangonzagaoliveira@gmail.com>
2025-08-14 05:17:05 -0300

    Cria uma entidade no banco de dados a partir de PessoaModel

---
+++ src/main/java/com/luanliv/SpringProject/PessoaModel.java
@@ -0,0 +1,50 @@
+package com.luanliv.SpringProject;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public PessoaModel(){
+
+    }
+
+    public PessoaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
